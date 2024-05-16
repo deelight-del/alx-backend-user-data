4c31351ca4dcedd2187c7de0de62c18b8fabceba
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-16 11:43:55 +0000

    reduced number of lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,9 +13,5 @@ def filter_datum(fields: typing.Sequence,
     """Function to implement obfustication based
     on the redaction and field string
     """
-    obfusticated_message = message
-    for field in fields:
-        obfusticated_message = re.sub(field + r'=.*?' + seperator,
-                                      field + r'=' + redaction + seperator,
-                                      obfusticated_message)
-    return obfusticated_message
+    pattern = r"(" + "|".join(fields) + r")" + r"(=.*?)" + seperator
+    return re.sub(pattern, r"\1=" + redaction + seperator, message)
