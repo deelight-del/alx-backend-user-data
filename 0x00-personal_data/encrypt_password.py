8e087803831b2feb907e7eb84da1d0400c7a6668
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-16 18:22:09 +0000

    Task 5

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Hashing password module with the bcrypt tool"""
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """Function to return a salted, hashed password"""
+    return bcrypt.hashpw(password, bcrypt.gensalt())
