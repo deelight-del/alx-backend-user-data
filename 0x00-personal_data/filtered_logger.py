3391dea4ec91ec85b8e1465e5eb1ba40e263e170
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-16 11:48:15 +0000

    fix type issues

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ import re
 
 
 def filter_datum(fields: typing.Sequence, redaction: str, message: str,
-                 seperator: str):
+                 seperator: str) -> str:
     """Function to implement obfustication based"""
     pattern = r"(" + "|".join(fields) + r")" + r"(=.*?)" + seperator
     return re.sub(pattern, r"\1=" + redaction + seperator, message)
