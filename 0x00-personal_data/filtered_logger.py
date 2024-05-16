14493f369d71ba594ec704203c11f25e0fdddc68
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-16 12:01:21 +0000

    xxxx

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,5 +9,6 @@ import re
 def filter_datum(fields: typing.List[str], redaction: str, message: str,
                  seperator: str) -> str:
     """Function to implement obfustication based"""
-    pattern = r"(" + "|".join(fields) + r")" + r"(=.*?)" + seperator
-    return re.sub(pattern, r"\1=" + redaction + seperator, message)
+    if fields:
+        pattern = r"(" + "|".join(fields) + r")" + r"(=.*?)" + seperator
+        return re.sub(pattern, r"\1=" + redaction + seperator, message)
