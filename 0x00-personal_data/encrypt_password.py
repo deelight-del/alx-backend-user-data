281707a2e9b86caa34b4a362ea7b33f3e436b1e0
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-16 18:32:45 +0000

    xxxx

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,6 +6,9 @@ import bcrypt
 import typing
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """Function to return a salted, hashed password"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+#def is_valid(hashed_password: bytes)
