3a80197823870976385e4219230c274c591ff952
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-20 18:32:29 +0000

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ class
 
 
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+import binascii
 
 
 class BasicAuth(Auth):
@@ -20,3 +22,15 @@ class BasicAuth(Auth):
            or not authorization_header.startswith("Basic ")):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """Method to decode a base64 encoding to utf-8"""
+        if (base64_authorization_header is None
+           or not isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except binascii.Error:
+            return None
