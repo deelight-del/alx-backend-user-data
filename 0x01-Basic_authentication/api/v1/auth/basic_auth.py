c1b9c1801e054707cd2662328b371e82efaf2dec
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-21 10:45:19 +0000

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,8 @@ class
 from api.v1.auth.auth import Auth
 from base64 import b64decode
 import binascii
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -46,3 +48,21 @@ class BasicAuth(Auth):
            or ":" not in decoded_base64_authorization_header):
             return None, None
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """The method to construct a user object from given credentials
+        if the credentials are valid
+        """
+        if (user_email is None or
+           not isinstance(user_email, str)
+           or user_pwd is None
+           or not isinstance(user_pwd, str)):
+            return None
+        User.load_from_file()
+        users_with_user_email = User.search({"email": user_email})
+        if not users_with_user_email:
+            return None
+        if not users_with_user_email[0].is_valid_password(user_pwd):
+            return None
+        return users_with_user_email[0]
