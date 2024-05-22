2829f1490e59abbb5a9577a8ccf252c77516ef48
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-22 17:58:28 +0000

    fix for task 2, 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,7 +20,7 @@ class SessionAuth(Auth):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
-        #print("dictionary here, when creating session",
+        # print("dictionary here, when creating session",
         #     self.__class__.user_id_by_session_id)
         return session_id
 
