9996c603aa396984ac3636c785a65bebbdf88253
deelight-del <akingbenidavidoyin@gmail.com>
2024-05-22 17:54:28 +0000

    Task 2, 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,8 +20,8 @@ class SessionAuth(Auth):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
-        print("dictionary here, when creating session",
-              self.__class__.user_id_by_session_id)
+        #print("dictionary here, when creating session",
+        #     self.__class__.user_id_by_session_id)
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
@@ -30,7 +30,7 @@ class SessionAuth(Auth):
         if (session_id is None
            or not isinstance(session_id, str)):
             return None
-        print("dictionary here", self.__class__.user_id_by_session_id)
+        # print("dictionary here", self.__class__.user_id_by_session_id)
         return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
